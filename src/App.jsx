bdff80b1f893a160f3cad3843d17bb6404a38871
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-25 17:26:35 +0200

    feat: create component blue button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
+import { styled, Typography } from "@mui/material";
 
+const BlueButton = styled(Button)({
+  backgroundColor: "skyblue",
+  color: "#888",
+  margin: "5px",
+  "&:hover": {
+    backgroundColor: "lightblue",
+  },
+  "&:disabled": {
+    backgroundColor: "gray",
+    color: "white",
+  },
+});
 function App() {
   return (
     <>
-      <Button variant="contained" color="secondary" startIcon=<AcUnitIcon />>
+      <Button variant="contained" color="secondary" startIcon={<AcUnitIcon />}>
         Hello world
       </Button>
       <Button disabled variant="contained">
         Hello world
       </Button>
+      <Typography variant={"h1"} component={"p"}>
+        Hello World !
+      </Typography>
+      <BlueButton>My Button 1</BlueButton>
+      <BlueButton>My Button 2</BlueButton>
     </>
   );
 }
