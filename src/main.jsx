e5f45a48db042057e132c3544bddacececb824e2
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-25 17:33:59 +0200

    feat: added theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
---
+++ src/theme.jsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1760a5",
+      light: "red",
+    },
+    secondary: {
+      main: "#ffffff",
+      light: "green",
+    },
+  },
+});
