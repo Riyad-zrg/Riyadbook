62b81197c49d1e60710fd1415b624b0bfcf2c578
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-25 17:49:28 +0200

    feat: added components for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import AcUnitIcon from "@mui/icons-material/AcUnit";
-import { styled, Typography } from "@mui/material";
+import {Sidebar, Feed, Rightbar} from "./components"
 
-const BlueButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  color: "#888",
-  margin: "5px",
-  "&:hover": {
-    backgroundColor: "lightblue",
-  },
-  "&:disabled": {
-    backgroundColor: "gray",
-    color: "white",
-  },
-}));
 function App() {
-  return (
-    <>
-      <Button variant="contained" color="secondary" startIcon={<AcUnitIcon />}>
-        Hello world
-      </Button>
-      <Button disabled variant="contained">
-        Hello world
-      </Button>
-      <Typography variant={"h1"} component={"p"}>
-        Hello World !
-      </Typography>
-      <BlueButton>My Button 1</BlueButton>
-      <BlueButton>My Button 2</BlueButton>
-    </>
-  );
+  return (<>
+  <Sidebar />
+  <Feed />
+  <Rightbar />
+  </>);
 }
 
 export default App;
