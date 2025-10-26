cd9f155f41d01c31c15b954257042c2a2a90044e
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-26 11:53:50 +0100

    transferred post in its own component and added multiple instances in Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,63 +1,14 @@
 import React from "react";
-import {
-  Avatar,
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Checkbox,
-  IconButton,
-  Typography,
-} from "@mui/material";
-import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Box } from "@mui/material";
+import Post from "./Post.jsx";
 
 function Feed() {
   return (
     <Box flex={4} p={2}>
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          component="img"
-          height="20%"
-          image="src/assets/images/posts/post_image_grand_rex.jpg"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            #ConcoursJeFilmeLeMétierQuiMePlait2024 #GrandRex Bravo à nos deux
-            gagnant du Grand Prix Éducation !
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <Checkbox
-              icon={<FavoriteBorder />}
-              checkedIcon={<FavoriteIcon sx={{ color: "red" }} />}
-            />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </Box>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,62 @@
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import React from "react";
+
+function Post() {
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="20%"
+        image="src/assets/images/posts/post_image_grand_rex.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          #ConcoursJeFilmeLeMétierQuiMePlait2024 #GrandRex Bravo à nos deux
+          gagnant du Grand Prix Éducation !
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<FavoriteIcon sx={{ color: "red" }} />}
+          />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default Post;
