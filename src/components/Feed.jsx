addb70e72801686ae334da4d512948b366ba4fc0
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-26 11:43:26 +0100

    added a post in feed with image

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,59 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { red } from "@mui/material/colors";
 
 function Feed() {
   return (
     <Box style={{ backgroundColor: "skyblue" }} flex={4} p={2}>
-      Feed
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="450"
+          image="src/assets/images/posts/post_image_grand_rex.jpg"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            #ConcoursJeFilmeLeMétierQuiMePlait2024 #GrandRex Bravo à nos deux
+            gagnant du Grand Prix Éducation !
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
     </Box>
   );
 }
