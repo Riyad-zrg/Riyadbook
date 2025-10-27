bce793ece8a9ee031ef2506396a9cf865deb18e7
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-27 14:27:36 +0100

    feat : modal opening when clickind add button

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,20 +1,33 @@
-import { Fab, Tooltip } from "@mui/material";
+import { Box, Button, Fab, Modal, Tooltip } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import React from "react";
 function Add() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
       <Tooltip
-        title="Add"
+        title="Nouveau Post"
         sx={{
           position: "fixed",
           bottom: "20px",
           left: { xs: "calc(50% - 25px)", md: "30px" },
         }}
+        onClick={handleOpen}
       >
         <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
       </Tooltip>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>Hello</Box>
+      </Modal>
     </>
   );
 }
