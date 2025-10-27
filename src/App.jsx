ac4be67351f866f03ffde45ffc3035d846d17f7c
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-27 15:53:57 +0100

    feat: added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import Sidebar from "./components/Sidebar.jsx";
 import Feed from "./components/Feed.jsx";
 import Rightbar from "./components/Rightbar.jsx";
-import { Box, Stack } from "@mui/material";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
 import Navbar from "./components/Navbar.jsx";
 import Add from "./components/Add.jsx";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("dark");
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-      <Add />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box backgroundColor={"background.default"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
