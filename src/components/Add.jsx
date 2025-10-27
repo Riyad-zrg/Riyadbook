924899e9d1074c15e44660af5b2e0324e71ba854
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-10-27 15:33:01 +0100

    feat : added TextField in add post modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,10 +1,34 @@
-import { Box, Button, Fab, Modal, Tooltip } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Fab,
+  Modal,
+  styled,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import React from "react";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "10px",
+}));
+
 function Add() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
   return (
     <>
       <Tooltip
@@ -20,14 +44,42 @@ function Add() {
           <AddIcon />
         </Fab>
       </Tooltip>
-      <Modal
+      <StyledModal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box>Hello</Box>
-      </Modal>
+        <Box
+          width={800}
+          height={520}
+          backgroundColor="white"
+          padding={3}
+          borderRadius={5}
+        >
+          <Typography variant={"h5"} color={"gray"} textAlign={"center"}>
+            Créer un nouveau post
+          </Typography>
+          <UserBox>
+            <Avatar
+              sx={{ width: "30px", height: "30px" }}
+              alt="Avatar"
+              src="https://media.licdn.com/dms/image/v2/D4E03AQHyxEVxYejbsw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1725607631430?e=2147483647&v=beta&t=z7BJKnTdQmBANNl3lik6dqXQXSg7JHIRfvWe4zSzeR8"
+            />
+            <Typography fontWeight={500} variant={"span"}>
+              Riyad Zerrougui
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={4}
+            placeholder="Quoi de neuf, Riyad ?"
+            variant="standard"
+          />
+        </Box>
+      </StyledModal>
     </>
   );
 }
